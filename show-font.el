7d1cf73686c03f24d06ba2bf143b14579e5f490a
Protesilaos Stavrou <info@protesilaos.com>
2024-09-10 08:29:52 +0300

    Update to show-font version 0.1.0

--- show-font.el
+++ show-font.el
@@ -5,7 +5,7 @@
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; Maintainer: Protesilaos Stavrou <info@protesilaos.com>
 ;; URL: https://github.com/protesilaos/show-font
-;; Version: 0.0.0
+;; Version: 0.1.0
 ;; Package-Requires: ((emacs "28.1"))
 ;; Keywords: convenience, writing, font
 
